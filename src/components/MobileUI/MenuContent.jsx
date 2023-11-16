9aaab22e5b57bcc957e5b12125a331cf022c2ba2
Onursml <crzyonur5@gmail.com>
2023-11-16 03:52:12 +0300

    ADSADq

--- src/components/MobileUI/MenuContent.jsx
+++ src/components/MobileUI/MenuContent.jsx
@@ -5,7 +5,7 @@ function MenuContent() {
     const visibile=useSelector(state=>state.mobileco.bar)
     console.log(visibile)
   return (
-    <div className={`fixed z-40 w-[90%] h-full transition-all duration-500 bg-anarenk2  sm:hidden bg-opacity-50 backdrop-blur-md ${visibile ? "w-[90%]" : "w-0" }`}>
+    <div className={`fixed z-40  h-full transition-all duration-500 bg-anarenk2  sm:hidden bg-opacity-50 backdrop-blur-md ${visibile ? "w-[90%]" : "w-0" }`}>
       
     </div>
   )
