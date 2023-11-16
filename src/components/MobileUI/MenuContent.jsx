305e149fb483882010c07d61804389f33276afec
Onursml <crzyonur5@gmail.com>
2023-11-16 04:03:52 +0300

    sadWQEDWECS

--- src/components/MobileUI/MenuContent.jsx
+++ src/components/MobileUI/MenuContent.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import './Mobile.css'
+
 
 function MenuContent() {
     const visibile=useSelector(state=>state.mobileco.bar)
     console.log(visibile)
   return (
-    <div className={`fixed z-40  h-full transition-all duration-500 bg-anarenk2  sm:hidden bg-opacity-50 backdrop-blur-md ${visibile ? "w-[90%]" : "w-0" }`}>
-      
+    <div className={`fixed z-40 blr  h-full transition-all duration-500  sm:hidden   ${visibile ? "w-[90%]" : "w-0" }`}>
+      sadasdasd
     </div>
   )
 }
